9976368787013a6add505904474d4b5f01ea6f62
Vinay <vjanard@gmail.com>
2019-09-26 20:26:23 -0500

    Home and about templates with title variables

--- livewell.py
+++ livewell.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Vinay Janardhanam',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 28, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 29, 2019'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 
