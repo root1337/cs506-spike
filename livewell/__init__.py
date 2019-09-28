10f056717117263247ca46bccc2d6819fdcd4612
Vinay <vjanard@gmail.com>
2019-09-28 12:44:42 -0500

    Login and Register pages working

--- livewell/__init__.py
+++ livewell/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '718b3887c64a3f564e8fdb398c2af627'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from livewell import routes
