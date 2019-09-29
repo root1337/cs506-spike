f6e5acb56cb54222ccdf41de97ff3ba87c55ae08
Vinay <vjanard@gmail.com>
2019-09-28 20:19:14 -0500

    user can create posts

--- livewell/routes.py
+++ livewell/routes.py
@@ -3,28 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from livewell import app, db, bcrypt
-from livewell.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from livewell.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from livewell.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Vinay Janardhanam',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 28, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 29, 2019'
-    }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -76,7 +63,7 @@ def save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-    #resize picture
+    # resize picture
     output_size = (125, 125) 
     i = Image.open(form_picture) 
     i.thumbnail(output_size)
@@ -102,3 +89,16 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
