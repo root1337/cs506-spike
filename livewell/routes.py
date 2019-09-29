1455e65cac88eb36cd5b76406a870cba8c5ddf5f
Vinay <vjanard@gmail.com>
2019-09-29 15:42:12 -0500

    Changed some names to avoid linting warnings

--- livewell/routes.py
+++ livewell/routes.py
@@ -105,7 +105,7 @@ def new_post():
 
 
 @app.route('/post/<int:post_id>')
-def post(post_id):
+def postPage(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
@@ -121,7 +121,7 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Your post has been updated', 'success')
-        return redirect(url_for('post', post_id=post.id))
+        return redirect(url_for('postPage', post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
